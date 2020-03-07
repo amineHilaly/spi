45cfe06bea64d58e4c297cbc4c1d95466dd00fc7
Asmae Atmani <asmae96atmani@gmail.com>
2020-03-07 13:02:08 +0100

    Second Commit

--- src/main/java/main/model/business/EtudiantBusiness.java
+++ src/main/java/main/model/business/EtudiantBusiness.java
@@ -1,13 +1,48 @@
 package main.model.business;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import main.model.entities.Etudiant;
 import main.model.repositories.EtudiantRepository;
 
 @Resource
 public class EtudiantBusiness {
 	@Autowired
 	private EtudiantRepository etudiantRepo;
+	
+	public void creerEtudiant(List<Etudiant> etudiant) {
+		etudiantRepo.saveAll(etudiant );
+	}
+	
+	public void updateEtudiant(Etudiant etudiant) {
+		etudiantRepo.save(etudiant);
+	}
+	
+	
+	public Collection<Etudiant> getAllEtudiants(){
+		return (Collection<Etudiant>) etudiantRepo.findAll();
+	}
+	
+	
+	public Optional<Etudiant> getEtudiantById(String noEtudiant) {
+		return etudiantRepo.findById(noEtudiant);
+	}
+	
+	
+	public void deleteEtudiantById(String noEtudiant) {
+		etudiantRepo.deleteById(noEtudiant); 
+		
+	}
+	
+	public void deleteAllEtudiants() {
+		etudiantRepo.deleteAll();
+	}
+	
+	
 }
---
+++ src/main/java/main/model/service/EtudiantWebService.java
@@ -0,0 +1,69 @@
+package main.model.service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import main.model.business.EtudiantBusiness;
+import main.model.entities.Etudiant;
+
+@RestController
+@RequestMapping("Etudiant")
+public class EtudiantWebService {
+	
+	@Autowired
+	EtudiantBusiness etudiantBusiness;
+	
+	
+	@GetMapping(path="/etudiants", produces = "application/json")
+	public Collection<Etudiant> getAll(){
+		return etudiantBusiness.getAllEtudiants();
+		
+	}
+	
+	@GetMapping(path="/{noEtudiant}", produces = "application/json")
+	public Optional<Etudiant> getEtudiantById(@PathVariable String noEtudiant) {
+		return etudiantBusiness.getEtudiantById(noEtudiant);
+	}
+	
+	@PostMapping(consumes = "application/json", produces = "application/json")
+	 public void creerEtudiant(@RequestBody List<Etudiant> etudiant) {
+		etudiantBusiness.creerEtudiant(etudiant);
+	}
+	
+	@PutMapping()
+	public void updateEtudiant(@RequestBody Etudiant etudiant) {
+		etudiantBusiness.updateEtudiant(etudiant);
+	}
+	
+	@DeleteMapping("/{noEtudiant}")
+	public @ResponseBody ResponseEntity<String> deleteEtudiant(@PathVariable String noEtudiant) {
+		etudiantBusiness.deleteEtudiantById(noEtudiant);
+	    return new ResponseEntity<String>("Etudiant a été suprimé", HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/all")
+	public @ResponseBody ResponseEntity<String> deleteAllEtudiants() {
+		etudiantBusiness.deleteAllEtudiants();
+	    return new ResponseEntity<String>("Etudiants ont été suprimé", HttpStatus.OK);
+	}
+	
+
+}
