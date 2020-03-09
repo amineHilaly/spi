abf81d2f0d9a9c7ed4ec883e5d2a69bccf060360
meedmakhlouf <makhlouf.mohamed.dev@gmail.com>
2020-03-09 17:22:00 +0100

    last commit promotion

---
+++ src/main/java/main/model/service/ProcessusStageWebService.java
@@ -0,0 +1,38 @@
+package main.model.service;
+
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import main.model.business.QualificatifBusiness;
+import main.model.entities.ProcessusStage;
+import main.model.entities.Qualificatif;
+import main.model.repositories.ProcessusStageRepository;
+
+
+@RestController
+@RequestMapping("/Domaines")
+public class ProcessusStageWebService {
+	
+	
+	@Autowired
+	ProcessusStageRepository ProcessStage;
+	
+	@GetMapping()
+	public List <ProcessusStage> getAll() {
+		return (List<ProcessusStage>) ProcessStage.findAll();
+	}
+	
+	
+}
