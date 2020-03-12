576780b2b41baa1bddcc3c8fddd4f1165eae981b
amineHilaly <amine.inie@gmail.com>
2020-03-12 15:49:42 +0100

    v1.8.5

--- src/main/java/main/model/service/QualificatifWebService.java
+++ src/main/java/main/model/service/QualificatifWebService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,45 +20,66 @@ import main.model.business.QualificatifBusiness;
 import main.model.entities.Qualificatif;
 import main.model.repositories.EtudiantRepository;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("Qualificatif")
+@RequestMapping(value = "Qualificatif" , produces = "application/json; charset=utf-8")
 public class QualificatifWebService {
 	
 	
 	@Autowired
 	QualificatifBusiness business;
 	
-	@Autowired
-	EtudiantBusiness etudiantBusiness;
 	
 	@GetMapping()
+	@RequestMapping(produces = "application/json; charset=utf-8")
 	public List <Qualificatif> getAll() {
-		return business.getAll();
+		try {
+			return business.getAll();
+		}catch (Exception e) {
+			return null;
+		}
 	}
 	
 	@GetMapping("/{idsent}")
-	public Qualificatif getId(@PathVariable long idsent) {
-		Long id = new Long(idsent);
-		return business.getById(id);
+	public Qualificatif getById(@PathVariable long idsent) {
+		try {
+			return business.getById(Long.valueOf(idsent));
+		}catch (Exception e) {
+			return null;
+		}
 	}
 	
 	@DeleteMapping("/{id}")
 	public boolean delete(@PathVariable long id) {
-		Qualificatif qualifactif = new Qualificatif();
-		qualifactif.setIdQualificatif(id);
-		return business.delete(qualifactif);
+		try {
+			Qualificatif qualifactif = new Qualificatif();
+			qualifactif.setIdQualificatif(id);
+			boolean res = business.delete(qualifactif); 
+			return res;
+		}catch (Exception e) {
+			return false;
+		}
 	}
 	
 	@PostMapping()
-	public void create(@RequestBody Qualificatif qualificatif) {
-		business.create(qualificatif);
+	public boolean create(@RequestBody Qualificatif qualificatif) {
+		try {
+			business.create(qualificatif);
+			return true;
+		}catch (Exception e) {
+			System.out.println(e);
+			return false;
+		}
 	}
 	
 	
 	@PutMapping()
-	public void update(@RequestBody Qualificatif qualificatif) {
-		business.update(qualificatif);
+	public boolean update(@RequestBody Qualificatif qualificatif) {
+		try {
+			return business.update(qualificatif);
+		}catch (Exception e) {
+			return false;
+		}
 	}
 	
 }
