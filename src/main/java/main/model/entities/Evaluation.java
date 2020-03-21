1cbafeedcf58543c3d12b0faf99069b5bfa5f5b8
meedmakhlouf <makhlouf.mohamed.dev@gmail.com>
2020-03-21 16:40:56 +0100

    last com

--- src/main/java/main/model/entities/Evaluation.java
+++ src/main/java/main/model/entities/Evaluation.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package main.model.entities;
 
 import java.io.Serializable;
@@ -173,34 +173,3 @@ public class Evaluation implements Serializable {
 	}
 
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package main.model.entities;
-
-import java.io.Serializable;
-import java.util.Collection;
-import java.util.Date;
-import javax.persistence.Basic;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
-
->>>>>>> ebd217bd93ae0ceadaa5247118b27c20d0e2d8ec
