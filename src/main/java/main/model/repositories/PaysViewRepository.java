6d2570361d9adb3338e967e07b9a1223bb391278
amineHilaly <amine.inie@gmail.com>
2020-03-26 19:35:26 +0100

    delete all

--- src/main/java/main/model/repositories/PaysViewRepository.java
+++
@@ -1,9 +0,0 @@
-package main.model.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import main.model.entities.PaysView;
-
-public interface PaysViewRepository extends CrudRepository<PaysView, String>{
-
-}
--- src/main/java/main/model/repositories/UniversiteViewRepository.java
+++
@@ -1,9 +0,0 @@
-package main.model.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import main.model.entities.UniversiteView;
-
-public interface UniversiteViewRepository extends CrudRepository<UniversiteView, String>{
-
-}
